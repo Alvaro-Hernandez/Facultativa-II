c1a0e0fa289dcabec3b33576619595ddfb504aff
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-04-12 00:15:49 -0600

    Simplificando los Ejercicios antes resueltos(1,2,3)

--- 11-04-Ejercicios.js
+++ 11-04-Ejercicios.js
@@ -1,12 +1,12 @@
 // Ejercicios asignados a mi Persona.
 // Ejercicio 10
-const numeroConfirmacion = (num) => num > 0 ? console.log('true') : console.log('false');   
+const numeroConfirmacion = (num) => num > 0 ? console.log(true) : console.log(false);   
 
 // Ejecución de la funcion - Éxito✅
 numeroConfirmacion(6);
 
 // Ejercicio 29
-const condicionNumeros = (num1, num2, bandera) => bandera === true ? console.log(Math.pow(num1, num2)) : console.log(Math.sqrt(num1));
+const condicionNumeros = (num1, num2, bandera) => bandera === true ? console.log( Math.pow(num1, num2) ) : console.log( Math.sqrt(num1) );
 
 // Ejecución de la funcion - Éxito✅
 condicionNumeros(3, 3, false);
@@ -16,31 +16,22 @@ condicionNumeros(3, 3, false);
 // Ejercicios Propuestos de toda la Clase 😁🐣
 
 // Ejercicio 1😀
-const sumaNumeros = (num1, num2) =>{
-    console.log(num1+num2);
-}
-
+const sumaNumeros = (num1, num2) => console.log( num1 + num2 ) ;
 // Ejecución de la funcion - Éxito✅
-SUMA(1,2);
+sumaNumeros(1,2);
 
 // Ejercicio 2😁
-const tamañoCadena = (palabra) =>{
-    console.log(palabra.length);
-}
-
+const tamañoCadena = (palabra) => console.log(palabra.length);
 // Ejecución de la funcion - Éxito✅
 tamañoCadena('Hola Mundo');
 
 // Ejercicio 3
-const sumaArreglos = (array) =>{
-    let element = 0;
-    for (let i = 0; i < array.length; i++) {
-        element += array[i];
-    }
-    console.log(element);
-}
-
 //Declaramos el Arreglo
 let array = [10,20,30];
+const sumaArreglos = (array) =>{
+    let total = array.reduce((valorAnterior, valorActual) => valorAnterior + valorActual );
+    console.log(total);
+}
+
 // Ejecución de la funcion - Éxito✅
 sumaArreglos(array);
